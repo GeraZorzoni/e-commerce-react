41e153e9a8fd957b08c8e2383efb617e31ef7cda
GeraZorzoni <gzorzoni@gmail.com>
2024-01-10 16:24:15 -0300

    Trabajo sobre map en carrito

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -1,7 +1,21 @@
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+
 export default function Carrito() {
+  const { carrito } = useContext(CartContext);
   return (
     <div className='container'>
       <h1 className='main-title'>Carrito</h1>
+      <br />
+      {carrito.map((producto) => (
+        <div key={producto.id}>
+          <h2>{producto.titulo}</h2>
+          <p>Precio Unitario: $ {producto.precio}</p>
+          <p>Precio Total: $ {producto.precio * producto.cantidad}</p>
+          <p>Cant: {producto.cantidad}</p>
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
